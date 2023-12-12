f729cbcd0450dfae975218444b83e7a3a3639b3b
Totom08 <65450398+Totom08@users.noreply.github.com>
2023-12-12 11:57:05 +0100

    Update testinfotp.ino

--- testinfotp.ino
+++ testinfotp.ino
@@ -1,103 +1,95 @@
-/* This file has been prepared for Doxygen automatic documentation generation.*/
-/*! \file *********************************************************************
- *
- * \brief
- *      Transmition d'un code ASCII à l'aide d'Arduino
- *
+#define F_CPU 16000000UL // Fréquence du CPU
+#define BAUD 9600 // Débit en bauds
+#define MYUBRR F_CPU/16/BAUD-1 // Calcul de la valeur d'UBRR
 
- *
- * \par Note :  Fichier à partir de la Datasheet
- *
- * \par Documentation : https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
- * 
- * \author
- *      Github: https://github.com/Totom08 \n
- *      email: thomas.davenne@etudiant.univ-reims.fr
- *
- * $Name: LiaisonSerie_RELEASE_1_0 $
- * $Revision: 1.8 $
- * $RCSfile: testinfotp.c,v $
- * $Date: 2023/11/22 11:02:21 $  \n
- * 
- */
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
-#define FOSC 16000000 // Clock Speed
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-int flag=0;
+volatile int flag3 = 0,flag0=0; // Variable pour indiquer la réception de données
+volatile char donnee; // Variable pour stocker les données reçues
 
-/*! \brief Fonction Initialisation
- */
-void USART_Init(unsigned int ubrr)
-{
-/*Set baud rate */
-UBRR0H = (unsigned char)(ubrr>>8);
-UBRR0L = (unsigned char)ubrr;
-/*Enable receiver and transmitter */
-UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
-/* Set frame format: 8data, 2stop bit */
-UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+// Initialisation de l'USART0
+void USART0_Init(unsigned int ubrr) {
+  /* Configuration du taux de bauds */
+  UBRR0H = (unsigned char)(ubrr >> 8);
+  UBRR0L = (unsigned char)ubrr;
+  // Activation du récepteur et du transmetteur
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+  /* Configuration du format de trame : 8 bits de données, 1 bit de stop */
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-/*! \brief Fonction Transmition
- */
-void USART_Transmit(unsigned char data)
-{
-/* Wait for empty transmit buffer */
-while (!(UCSR0A & (1<<UDRE0)))
-;
-/* Put data into buffer, sends the data */
-UDR0 = data;
+// Initialisation de l'USART3
+void USART3_Init(unsigned int ubrr) {
+  /* Configuration du taux de bauds */
+  UBRR3H = (unsigned char)(ubrr >> 8);
+  UBRR3L = (unsigned char)ubrr;
+  // Activation du récepteur et du transmetteur
+  UCSR3B = (1 << RXEN3) | (1 << TXEN3) | (1 << RXCIE3);
+  /* Configuration du format de trame : 8 bits de données, 1 bit de stop */
+  UCSR3C =  (1 << UCSZ31) | (1 << UCSZ30);
 }
 
-/*! \brief Fonction Recevoir
- */
-unsigned char USART_Receive(void)
-{
-/* Wait for data to be received */
-while (!(UCSR0A & (1<<RXC0)))
-;
-/* Get and return received data from buffer */
-return UDR0;
+// Transmission via USART0
+void USART0_Transmit(unsigned char data) {
+  /* Attente du buffer de transmission vide */
+  while (!(UCSR0A & (1 << UDRE0)));
+  /* Envoi des données dans le buffer */
+  UDR0 = data;
 }
 
-/*! \brief Interruption
- */
-ISR(USART_RX_vect) //https://www.nongnu.org/avr-libc/user-manual/group__avr__interrupts.html
-{
-  flag=1;
+// Transmission via USART3
+void USART3_Transmit(unsigned char data) {
+  /* Attente du buffer de transmission vide */
+  while (!(UCSR3A & (1 << UDRE3)));
+  /* Envoi des données dans le buffer */
+  UDR3 = data;
 }
 
-/*! \brief Fonction Main
- */
-int main(void)
-{
+// Réception via USART0
+unsigned char USART0_Receive(void) {
+  /* Attente des données à recevoir */
+  while (!(UCSR0A & (1 << RXC0)));
+  /* Récupération et renvoi des données reçues */
+  return UDR0;
+}
 
-USART_Init(MYUBRR);
-sei();
-while(1){
-/*USART_Transmit('D');
-USART_Transmit('o');
-USART_Transmit('r');
-USART_Transmit('i');
-USART_Transmit('a');
-USART_Transmit('n');
-USART_Transmit(' ');
-USART_Transmit('C');
-USART_Transmit('H');
-USART_Transmit('A');
-USART_Transmit('U');
-USART_Transmit('V');
-USART_Transmit('E');
-USART_Transmit('T');
-USART_Transmit('\n');
-_delay_ms(1000);*/
-  if (flag==1){
-    USART_Transmit(UDR0);
-    flag=0;
+// Réception via USART3
+unsigned char USART3_Receive(void) {
+  /* Attente des données à recevoir */
+  while (!(UCSR3A & (1 << RXC3)));
+  /* Récupération et renvoi des données reçues */
+  return UDR3;
+}
+
+// Interruption pour la réception sur USART0
+ISR(USART0_RX_vect) {
+  flag0 = 1;
+}
+
+// Interruption pour la réception sur USART3
+ISR(USART3_RX_vect) {
+  flag3 = 1;
+  //PORTB = ~PINB;
+}
+
+int main(void) {
+  DDRB =0b10000000;
+  USART0_Init(MYUBRR); // Initialisation de l'USART0
+  USART3_Init(MYUBRR); // Initialisation de l'USART3
+  sei(); // Activation des interruptions globales
+  
+  while (1) {
+   //USART0_Transmit(USART3_Receive()); 
+    if (flag3) {
+      USART0_Transmit(UDR3); // Transmission des données reçues par USART3 via USART0
+      flag3 = 0;
+    }
+    if (flag0) {
+      USART3_Transmit(UDR0); // Transmission des données reçues par USART3 via USART0
+      flag0 = 0;
+    }
+    _delay_ms(1); // Délai d'attente
   }
-  _delay_ms(1);
-}
-
-
 }
