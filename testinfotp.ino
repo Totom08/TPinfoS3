b327e608bc5e9d87635409b2df2e12a4be7d444d
Totom08 <65450398+Totom08@users.noreply.github.com>
2023-12-19 14:21:25 +0100

    Update testinfotp.ino

--- testinfotp.ino
+++ testinfotp.ino
@@ -1,152 +1,105 @@
-/*! \file *********************************************************************
- *
- * \brief
- *      Transmition de données entre USART0 et USART3 sur Arduino
- *
- * \par Documentation :
- *      https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
- *
- * \author
- *      Github: https://github.com/Totom08 \n
- *      email: thomas.davenne@etudiant.univ-reims.fr
- *
- * $Name: LiaisonSerie_RELEASE_1_0 $
- * $Revision: 1.8 $
- * $RCSfile: testinfotp.c,v $
- * $Date: 2023/11/22 11:02:21 $  \n
- * 
- */
+#define FOSC 16000000  // Fréquence du Clock
+#define BAUD 9600      // Vitesse de transmission
+#define MYUBRR FOSC / 16 / BAUD - 1 // Calcul de l'UBRR
 
-#define F_CPU 16000000UL // Fréquence du CPU
-#define BAUD 9600 // Débit en bauds
-#define MYUBRR F_CPU/16/BAUD-1 // Calcul de la valeur d'UBRR
+#define FRAMING_ERROR (1 << FE0) // Erreur de trame
+#define PARITY_ERROR (1 << UPE0) // Erreur de parité
+#define DATA_OVERRUN (1 << DOR0) // Débordement de données
 
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
+#define RX_BUFFER_SIZE0 8 // Taille du buffer de réception
 
-volatile int flag3 = 0, flag0 = 0; // Variable pour indiquer la réception de données
-volatile char donnee; // Variable pour stocker les données reçues
+char rx_buffer0[RX_BUFFER_SIZE0]; // Déclaration du buffer
+unsigned char rx_wr_index0 = 0, rx_rd_index0 = 0; // Indices d'écriture et de lecture du buffer
+unsigned char rx_counter0 = 0; // Compteur de caractères dans le buffer
+bool rx_buffer_overflow0; // Variable pour indiquer le dépassement du buffer
 
-/*! \brief Initialisation de l'USART0
- *
- *  Configure l'USART0 avec la vitesse de transmission, le mode asynchrone,
- *  8 bits de données, 1 bit de stop.
- *
- *  \param ubrr Valeur du registre de baud rate pour la vitesse souhaitée.
- */
-void USART0_Init(unsigned int ubrr) {
-  /* Configuration du taux de bauds */
+/*! \brief Fonction Main */
+int main(void) {
+  USART_Init(MYUBRR); // Initialise la communication série
+  sei(); // Active les interruptions globales
+  //USART_Transmit(USART_Receive()+1); // Exemple de transmission et réception (actuellement commenté)
+  while (1) {
+    _delay_ms(1); // Attente nécessaire au fonctionnement
+  }
+}
+
+/*! \brief Fonction Initialisation de la communication */
+void USART_Init(unsigned int ubrr) {
+  /* Configuration de la vitesse de transmission */
   UBRR0H = (unsigned char)(ubrr >> 8);
   UBRR0L = (unsigned char)ubrr;
-  // Activation du récepteur et du transmetteur, Activation de l'interruption de réception
+  // Activation du récepteur et du transmetteur
   UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
-  /* Configuration du format de trame : 8 bits de données, 1 bit de stop */
-  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+  /* Configuration du format de trame: 8 bits de données, 2 bits d'arrêt */
+  UCSR0C = (1 << USBS0) | (3 << UCSZ00);
 }
 
-/*! \brief Initialisation de l'USART3
- *
- *  Configure l'USART3 avec la vitesse de transmission, le mode asynchrone,
- *  8 bits de données, 1 bit de stop.
- *
- *  \param ubrr Valeur du registre de baud rate pour la vitesse souhaitée.
- */
-void USART3_Init(unsigned int ubrr) {
-  /* Configuration du taux de bauds */
-  UBRR3H = (unsigned char)(ubrr >> 8);
-  UBRR3L = (unsigned char)ubrr;
-  // Activation du récepteur et du transmetteur, Activation de l'interruption de réception
-  UCSR3B = (1 << RXEN3) | (1 << TXEN3) | (1 << RXCIE3);
-  /* Configuration du format de trame : 8 bits de données, 1 bit de stop */
-  UCSR3C = (1 << UCSZ31) | (1 << UCSZ30);
-}
-
-/*! \brief Transmission via USART0
- *
- *  \param data Caractère à transmettre.
- */
-void USART0_Transmit(unsigned char data) {
+/*! \brief Fonction Transmission de la communication */
+void USART_Transmit(unsigned char data) {
   /* Attente du buffer de transmission vide */
-  while (!(UCSR0A & (1 << UDRE0)))
-    ;
-  /* Envoi des données dans le buffer */
+  while (!(UCSR0A & (1 << UDRE0)));
+  /* Mettre les données dans le buffer, envoie les données */
   UDR0 = data;
 }
 
-/*! \brief Transmission via USART3
- *
- *  \param data Caractère à transmettre.
- */
-void USART3_Transmit(unsigned char data) {
-  /* Attente du buffer de transmission vide */
-  while (!(UCSR3A & (1 << UDRE3)))
-    ;
-  /* Envoi des données dans le buffer */
-  UDR3 = data;
-}
-
-/*! \brief Réception via USART0
- *
- *  \return Caractère reçu.
- */
-unsigned char USART0_Receive(void) {
-  /* Attente des données à recevoir */
-  while (!(UCSR0A & (1 << RXC0)))
-    ;
-  /* Récupération et renvoi des données reçues */
+/*! \brief Fonction Reception de la communication */
+unsigned char USART_Receive(void) {
+  /* Attente de la réception des données */
+  while (!(UCSR0A & (1 << RXC0)));
+  /* Récupérer et retourner les données reçues du buffer */
   return UDR0;
 }
 
-/*! \brief Réception via USART3
- *
- *  \return Caractère reçu.
- */
-unsigned char USART3_Receive(void) {
-  /* Attente des données à recevoir */
-  while (!(UCSR3A & (1 << RXC3)))
-    ;
-  /* Récupération et renvoi des données reçues */
-  return UDR3;
-}
-
-/*! \brief Interruption de réception sur USART0
- *
- *  Cette interruption est déclenchée lorsqu'un caractère est reçu sur l'USART0.
- *  Elle indique la réception de données.
- */
-ISR(USART0_RX_vect) {
-  flag0 = 1;
-}
-
-/*! \brief Interruption de réception sur USART3
- *
- *  Cette interruption est déclenchée lorsqu'un caractère est reçu sur l'USART3.
- *  Elle indique la réception de données.
- */
-ISR(USART3_RX_vect) {
-  flag3 = 1;
-}
-
-/*! \brief Fonction principale
- *
- *  Fonction principale du programme.
- */
-int main(void) {
-  DDRB = 0b10000000; // Configuration de la broche PB7 en sortie
-  USART0_Init(MYUBRR); // Initialisation de l'USART0
-  USART3_Init(MYUBRR); // Initialisation de l'USART3
-  sei(); // Activation des interruptions globales
-
-  while (1) {
-    if (flag3) {
-      USART0_Transmit(UDR3); // Transmission des données reçues par USART3 via USART0
-      flag3 = 0;
-    }
-    if (flag0) {
-      USART3_Transmit(UDR0); // Transmission des données reçues par USART0 via USART3
-      flag0 = 0;
-    }
-    _delay_ms(1); // Délai d'attente
+/*! \brief Gestionnaire d'interruption pour la réception de données USART */
+ISR(USART_RX_vect) {
+  char status = UCSR0A;
+  char data = UDR0;
+  
+  // Remplir le buffer avec les données reçues
+  rx_buffer0[rx_wr_index0++] = data;
+  
+  // Vérifier si le pointeur d'écriture dépasse la taille du buffer
+  if (rx_wr_index0 == RX_BUFFER_SIZE0) {
+    rx_wr_index0 = 0; // Réinitialiser le pointeur d'écriture au début du buffer
   }
+  
+  // Gérer le dépassement du buffer
+  if (++rx_counter0 == RX_BUFFER_SIZE0) {
+    rx_counter0 = 0; // Réinitialiser le compteur
+    rx_buffer_overflow0 = true; // Indiquer le dépassement du buffer
+  }
+}
+
+/*! \brief Fonction Transmission d'une chaîne de caractères */
+void USART_puts(unsigned char *str) {
+  do {
+    USART_Transmit(*str); // Transmission caractère par caractère
+  } while (*++str != 0);
+}
+
+/*! \brief Fonction Transmission d'une chaîne de caractères avec un retour à la ligne */
+void USART_putsln(unsigned char *str) {
+  USART_puts(str); // Transmission de la chaîne
+  USART_Transmit(10); // Retour à la ligne
+  USART_Transmit(13); // Saut de ligne
+}
+
+// Ajouter une fonction pour lire les données du buffer
+char getBuffer(void) {
+  char data;
+  
+  while (rx_counter0 == 0); // Attendre qu'il y ait un caractère dans le buffer
+  
+  data = rx_buffer0[rx_rd_index0++];
+  
+  // Vérifier si le pointeur de lecture dépasse la taille du buffer
+  if (rx_rd_index0 == RX_BUFFER_SIZE0) {
+    rx_rd_index0 = 0; // Réinitialiser le pointeur de lecture au début du buffer
+  }
+  
+  cli(); // Désactiver les interruptions pendant la manipulation du compteur
+  --rx_counter0;
+  sei(); // Réactiver les interruptions
+  
+  return data;
 }
